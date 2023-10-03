6971f13fd6874c3ef59029e046f129f00a56be97
Wesley Araujo <wesley.araujo@alumni.carleton.ca>
2023-10-03 17:57:31 -0400

    templates: Extend index page to display posts by other (fake) users

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app # second app is variable app from __init__.py
 @app.route('/index')
 def index():
     user = {'username': 'Wesley'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
